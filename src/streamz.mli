832cf62a807aa01ac33fbc037a5f112b757602a4
Istvan Szukacs <istvan@streambrightdata.com>
2017-09-19 08:23:01 +0200

    Removing streams

--- src/streamz.mli
+++
@@ -1,6 +0,0 @@
-type 'a stream = Nil | Cons of 'a * (unit -> 'a stream)
-val hd : 'a stream -> 'a
-val tl : 'a stream -> 'a stream
-val nth : 'a stream -> int -> 'a
-val from_list : 'a list -> 'a stream
-val take : 'a stream -> int -> 'a list
